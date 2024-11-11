b2865889e0cf15641bdea35c2c574208fde3fb7a
soigroup <168447232+soigroup@users.noreply.github.com>
2024-11-11 07:30:39 +0500

    cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://your-vercel-app.vercel.app'],
+    'allowed_origins' => ['https://batcoin-six.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
