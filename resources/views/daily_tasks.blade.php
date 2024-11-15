f9f36d6441af0ce8f62d93856c83c638ec4aaf54
soigroup <168447232+soigroup@users.noreply.github.com>
2024-11-15 12:31:54 +0500

    daily_tasks.blade.php

--- resources/views/daily_tasks.blade.php
+++ resources/views/daily_tasks.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <h1 class="text-2xl font-bold mb-4">Daily Task List</h1>
-                    <a href="{{ route('create_daily_task') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4 inline-block">Create New Daily Task</a>
+                       <a href="{{ route('daily_tasks.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4 inline-block">Create New Daily Task</a>
                     <table class="min-w-full">
                         <thead>
                             <tr>
