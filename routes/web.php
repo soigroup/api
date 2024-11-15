2c149b716f92e03ca8f1e197f0a1c5b0a66a42fc
soigroup <168447232+soigroup@users.noreply.github.com>
2024-11-15 12:28:19 +0500

    web.php

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,17 @@ Route::get('/users', [AdminController::class, 'users'])->name('users');
 Route::get('/tasks', [AdminController::class, 'tasks'])->name('tasks');
 Route::get('/tasks/create', [AdminController::class, 'createTask'])->name('tasks.create');
 Route::post('/tasks', [AdminController::class, 'storeTask'])->name('tasks.store');
+Route::get('/tasks/{task}/edit', [AdminController::class, 'editTask'])->name('tasks.edit');
+Route::patch('/tasks/{task}', [AdminController::class, 'updateTask'])->name('tasks.update');
+Route::delete('/tasks/{task}', [AdminController::class, 'deleteTask'])->name('tasks.delete');
 
 // مسارات المهام اليومية
-Route::get('/daily-tasks', [AdminController::class, 'dailyTasks'])->name('daily_tasks.index');
+Route::get('/daily-tasks', [AdminController::class, 'dailyTasks'])->name('daily_tasks');
 Route::get('/daily-tasks/create', [AdminController::class, 'createDailyTask'])->name('daily_tasks.create');
 Route::post('/daily-tasks', [AdminController::class, 'storeDailyTask'])->name('daily_tasks.store');
+Route::get('/daily-tasks/{dailyTask}/edit', [AdminController::class, 'editDailyTask'])->name('daily_tasks.edit');
+Route::patch('/daily-tasks/{dailyTask}', [AdminController::class, 'updateDailyTask'])->name('daily_tasks.update');
+Route::delete('/daily-tasks/{dailyTask}', [AdminController::class, 'deleteDailyTask'])->name('daily_tasks.delete');
 
 // ملاحظة: تم إلغاء استيراد auth.php لأن المصادقة غير مطلوبة
 // require __DIR__.'/auth.php';
